22ebbf9e21dbceb08a78960b093db9c89034c0b6
Sarthak Munshi <sarthakmunshi@gmail.com>
2015-07-11 11:23:35 +0530

    JQuery Support Added

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,20 @@
+var peerflix = require('peerflix') ;
+var wjs = require("wcjs-player") ;
+var player = new wjs("#player").addPlayer({ autoplay: true }) ;
+
 $(document).ready(function() {
 
 	$('.button').click(function () {
+		var magnetLink = $('input[type=search]').val() ;
 		$('#magnetBox').addClass('animated fadeOutUp');
 		setTimeout(function() {
 			$('#magnetBox').hide() ;
 		},1000) ;
 
+		var engine = peerflix(magnetLink) ;
+		engine.server.on('listening', function() {
+    		var myLink = 'http://localhost:' + engine.server.address().port + '/';
+    		player.addPlaylist(myLink);
+		});
 	}) ;
 }) ;
--- static/js/torrent-server.js
+++ static/js/torrent-server.js
@@ -1,9 +1,9 @@
-var peerflix = require('peerflix'),
-    engine = peerflix('magnet:?xt=urn:btih:120ad76789b11bce2d6005d1c22ccf1507c3021a&dn=Pink+-+Just+Give+Me+A+Reason+%28Ft.+Nate+Ruess%29+M4A%2BMP4+%281080p%29+x2&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969'),
-    wjs = require("wcjs-player"),
-    player = new wjs("#player").addPlayer({ autoplay: true });
+var peerflix = require('peerflix') ;
+    var engine = peerflix(magnetLink) ;
+    var wjs = require("wcjs-player") ;
+    var player = new wjs("#player").addPlayer({ autoplay: true }) ;
 
 engine.server.on('listening', function() {
     var myLink = 'http://localhost:' + engine.server.address().port + '/';
     player.addPlaylist(myLink);
-});
+	});
